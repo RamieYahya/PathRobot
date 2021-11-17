d6bd2f322669c5bdb6fd6a45997cfca820db55e3
Ramie Yahya <ramie.yahya@gmail.com>
2021-11-17 10:20:00 -0500

    LDR Line Tracking Template

--- PathRobot.ino
+++ PathRobot.ino
@@ -4,20 +4,53 @@
 // IR Reciever
 int IR_RECEIVE_PIN = 12;
 
+// DC Motor
+const int ENABLE = 5;
+const int DIRA = 3;
+const int DIRB = 4;
+
+//LDRs
+const int LEFT_LDR = 6;
+const int RIGHT_LDR = 7;
+int leftReading = 0;
+int rightReading = 0;
+int whiteThreshold = 0;
+
 void setup() {
   IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK); // Start the receiver
   Serial.begin(9600);
+
+  // Set up motor pins direction and voltage
+  pinMode(ENABLE, OUTPUT);
+  pinMode(DIRA, OUTPUT);
+  pinMode(DIRB, OUTPUT);
+  digitalWrite(DIRA, LOW);
+  digitalWrite(DIRB, HIGH);
+  digitalWrite(ENABLE, LOW);
 }
 
+
 void loop() {
-  // no signal results = 0
- if (IrReceiver.decode()) {
-      Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
-      //IrReceiver.printIRResultShort(&Serial); // optional use new print version
-      
-      IrReceiver.resume(); // Enable receiving of the next value
+  leftReading = analogRead (LEFT_LDR);
+  rightReading = analogRead (RIGHT_LDR);
+
+  if (leftReading < whiteThreshold && rightReading < whiteThreshold) {
+    digitalWrite (ENABLE, HIGH);
+  } else if (leftReading >= whiteThreshold && rightReading >= whiteThreshold) {
+    digitalWrite (ENABLE, LOW);
+  } else if (leftReading >= whiteThreshold) {
+    // Turn right
+  } else {
+    // Turn Left
   }
 
-  
+
+  // no signal results = 0
+  if (IrReceiver.decode()) {
+    Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
+    //IrReceiver.printIRResultShort(&Serial); // optional use new print version
+
+    IrReceiver.resume(); // Enable receiving of the next value
+  }
 
 }
